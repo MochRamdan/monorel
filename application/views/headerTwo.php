e6fb8d19501ca4804ab979250d6fa305caeaa2b8
Moh Ramdan <muhammad.ramdhan68@gmail.com>
2020-04-08 12:02:26 +0700

    memperbaharui logo

--- application/views/headerTwo.php
+++ application/views/headerTwo.php
@@ -50,7 +50,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= base_url('Dashboard');?>" class="brand-link">
-      <img src="<?= base_url('assets/img/monorail.png')?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="<?= base_url('assets/img/monorel-white.png')?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">SI-monorel</span>
     </a>
